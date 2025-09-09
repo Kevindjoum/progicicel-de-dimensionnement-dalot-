225bda8adc3a7915aa9ffec63e0cd9ad5cb0f8bf
djoumessi woumpe kevin <86205547+Kevindjoum@users.noreply.github.com>
2025-09-09 01:16:22 +0100

    Add files via upload

---
+++ dalot_efforts_calc_Version2.py
@@ -0,0 +1,59 @@
+																							FORMULES DE CALCUL DES EFFORTS INTERNES EN FONCTION DU TYPE DE CHARGEMENT 															
+																							CHARGES UNIFORMEMENT APPLIQUEE SUR LA DALLE 				CHARGE TRIANGULAIRE SYMETRIQUE SUR LES DEUX COTES 				CHARGES CONCENTREES EGALES EN B ET C 				CHARGE UNIFORME SUR LES DEUX COTES			
+	CARACTERISTIQUES DES MATERIAUX 																			DESIGNATION DE LA CHARGE SUR LE DALOT			q (t/ml)				σ1(t/m²)		σ2(t/m²)		P (t)				σ(t/m²)			
+	BETON						ACIER 						SOL DE POUSSEE 							VALEUR 			1,529051988				0,407747197		1,291199456		0,611620795				0,305810398			
+	designation 			symbole 	valeur 	unité 	designation 			symbole 	valeur 	unité 	designation 			symbole 	valeur 	unité 		MOMENTS (t.m/ml)	MA		-0,091743119				-0,053618756				-0,076452599				-0,018348624			
+	Resistance caractéristiques à 28j				25	Mpa 	Limite d'elasticité 						poids volumique 			gama	18	KN/m3			MD		-0,091743119				-0,053618756				-0,076452599				-0,018348624			
+	Module d'elasticité 				22	Gpa	masse volumique 						angle de frottément interne 			phi	45	°			MB		-0,091743119				-0,048318043				0,01529052				-0,018348624			
+	Masse volumique 				25	KN/m2							cohesion			c	20	Kpa			MC		-0,091743119				-0,048318043				0,01529052				-0,018348624			
+	Coéfficient de poisson 				0,3								coef de poisson 			nu	0,3				M(B-C) (Tablier) 		0,183486239				-0,048318043				0,01529052				-0,018348624			
+	Resistance dimensionnement 				14,16666667	Mpa 							coef de poussé active			Ka	0,33				M(A-D) (Radier)		0,183486239				-0,053618756				0,107033639				-0,018348624			
+																					M(A-B) (piédroit gauche)		-0,091743119				0,128440367				-0,03058104				0,036697248			
+																					M(C-D) ( Piédroit droit)		-0,091743119				0,128440367				-0,03058104				0,036697248			
+	FORCE GRAVITATIONELLE			9,81	N/m²															EFFORT NORMAUX (t.m/ml)	S1		0				0,602446483				0,076452599				0,183486239			
+																					S2		0,917431193				0				0,611620795				0			
+																					S2'		0,917431193				0				0,611620795				0			
+																					S3		0				0,416921509				0,076452599				0,183486239			
+				DIMENSIONS ET CARACTERISTIQUES DU DALOT 																																		
+				DESIGNATION 				symbole 	valeur 	unité 		Paramètres  KLEINLOGEL																										
+				Largeur inrterne 				Li	1	m 		k1	1																									
+				Hauteur interne 				Hi	1	m 		k2	1																									
+				épaisseur dalle 				e_dalle	0,2	m 		K1	5							CHARGES DE REMBLAIS AU DESSUS DU TABLIER						POUSSEE LATTERALE DES TERRES 				SURCHARGE ROUTIERE SUR LES PIEDROITS				POIDS DES PIEDROITS 				
+				épaisssseur piédroits 				e_voile	0,2	m 		K2	5							Designation	epaisseur(m)	Poids vol (KN/m3)		charge q (KN/ml)		Designation		valeurs		Designation 		valeur		Désignation		Valeur		
+				épaisseur radier 				e_radier	0,2	m 		K3	3,8							Terre 	0,5	20		10		Poids volumique (KN/m3)		20		Charges q (KN/m²)		9		Poids propre piedroit P(KN)		6		
+				hauteur de calcul 				h	1,2	m 		K4	4,2							Chaussée	0	20		0		cohesion		0		pression latérale σ(KPa) 		3						
+				largeur de calcul 				l	1,2	m 		F1	24							Tablier	0,2	25		5		angle de frottement interne(°)		30										
+				momment d'inertie radier				j1	0,000666667	m4		F2	8										Total 	15		coef de poussée Ka		0,333333333										
+				momment d'inertie piédroits				j2	0,000666667	m4																Hauteur totale des remblais 		0,6										
+				moment d'innertie dalle 				j3	0,000666667	m4																σ1(Kpa)		4										
+																										σ2(Kpa)		12,66666667										
+																																						
+																																						
+																				CHARGES ROUTIRES 																		
+																				Désignation		Valeur de la charge sur un essieu (t)		Coef de charge (bc, bt)		Surface d'influence du pneu		Valeur de la charge q (t/m²)										
+																				Système de charge Bc 		12		1,1		2,1025		9,626634958										
+																				Système de charge Bt		16		1		2,61		6,130268199										
+																				Sytème de charge Br		10				2,7		3,703703704										
+																																						
+																						TABLEAU RECAPITULATIF DES RESULTATS DES EFFORTS INTERNES 																
+																					Designation 	MA	MD	MB	MC	M(B-C) Tablier	M(A-D) Radier	M(A-B) piedroit	S1	S2	S2'	S3						
+																				Charges sur tablier	1	-0,091743119	-0,091743119	-0,091743119	-0,091743119	0,183486239	0,183486239	-0,091743119	0	0,917431193	0,917431193	0						
+																				Pouids propre voile	2	-0,076452599	-0,076452599	0,01529052	0,01529052	0,01529052	0,107033639	-0,03058104	0,076452599	0,611620795	0,611620795	0,076452599						
+																				Poussée lattérale	3	-0,053618756	-0,053618756	-0,048318043	-0,048318043	-0,048318043	-0,053618756	0,128440367	0,602446483	0	0	0,416921509						
+																				Charge du convoie Bc	4	-0,577598098	-0,577598098	-0,577598098	-0,577598098	1,155196195	6,353579073	-0,577598098	0	5,775980975	5,775980975	0						
+																				Charge du convoie Bt	5	-0,367816092	-0,367816092	-0,367816092	-0,367816092	0,735632184	4,045977011	-0,367816092	0	3,67816092	3,67816092	0						
+																				Charge du convoe Br	6	-0,222222222	-0,222222222	-0,222222222	-0,222222222	0,444444444	2,444444444	-0,222222222	0	2,222222222	2,222222222	0						
+																				convoie de type max(BC, Bt, Br)	7=max(4,5,6)	-0,577598098	-0,577598098	-0,577598098	-0,577598098	1,155196195	6,353579073	-0,577598098	0	5,775980975	5,775980975	0						
+																				Surcharge routière	8	-0,018348624	-0,018348624	-0,018348624	-0,018348624	-0,018348624	-0,018348624	0,036697248	0,183486239	0	0	0,183486239						
+																				G	1+2+3	-0,221814475	-0,221814475	-0,124770642	-0,124770642	0,150458716	0,236901121	0,006116208	0,678899083	1,529051988	1,529051988	0,493374108						
+																				Q	7+8	-0,595946721	-0,595946721	-0,595946721	-0,595946721	1,136847571	6,335230449	-0,54090085	0,183486239	5,775980975	5,775980975	0,183486239						
+																				combinaison ELU (1,35G+1,5Q)		-1,193369623	-1,193369623	-1,062360449	-1,062360449	1,908390623	9,822662187	-0,803094394	1,191743119	10,72819165	10,72819165	0,941284404						
+																				combinaison ELS ( G+Q)		-0,817761196	-0,817761196	-0,720717364	-0,720717364	1,287306287	6,57213157	-0,534784642	0,862385321	7,305032963	7,305032963	0,676860347						
+																																						
+		CALCUL DU MUR EN AILE 																																				
+		Géométrie 			Paramètres du sol 			Calcul des charges appliquées 				force de poussée				Moment de dimensionnement 																						
+		Désigantioh 	Valeur 	Unité 	Désignation	Valeur 	Unité	Charges 	symbole	valeur	unité 	Désignation	symbole	Valeur 	unité 	Désignation		Valeur																				
+		Hauteur 	1,5	m	Poids vol 	18	KN/m3	Pression active	σsol	9	Kpa	Poussée du sol	Psol	6,75	KN	Moment max 		6,75																				
+		épasseur  voile	0,2	m	Angle frot 	30	°	Surchage 	σq	3	KN/m²	Poussée surcharge	Pq	4,5	KN	Moment de calcul (EELU)		9,1125																				
+					Cohésion	0		TOTAL	σTotale	12	Kpa 	TOTAL	Ptotale	11,25	KN																							
+					poussée active Ka	0,333333333																																
